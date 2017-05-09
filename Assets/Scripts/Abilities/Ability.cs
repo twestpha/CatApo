8c214def6c8cd6727b25fd56a83cf306f9856bd6
Trevor Westphal <trevorawestphal@gmail.com>
2017-05-08 21:14:17 -0700

    Added multiple placement feature, added more asserts to ability

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -9,6 +9,8 @@ public class Ability : ScriptableObject {
     // Defines an ability; abilities modify the world and the actors in it
     // when the actor who owns the ability casts it
 
+    public bool debug;
+
     public enum AbilitySource {
         Talent,
         Equipment,
@@ -27,7 +29,6 @@ public class Ability : ScriptableObject {
     public string abilityName;
 
     public AbilitySource source;
-    public AbilityPlacement placement;
     public bool includeSelf;
     public bool channeled; // TODO
 
@@ -36,6 +37,10 @@ public class Ability : ScriptableObject {
 
     // cast location stuff
     private Vector3 castPosition;
+
+    // Multiple placements
+    [Header("Placements")]
+    public List<AbilityPlacement> placements;
 
     [Header("Effects and Timing")]
     public List<AbilityEffect> effects;
@@ -56,33 +61,45 @@ public class Ability : ScriptableObject {
             effects[i] = UnityEngine.Object.Instantiate(effects[i]);
         }
 
-        placement = UnityEngine.Object.Instantiate(placement);
-
-        // Assert on the data
-        Assert.IsTrue(placement, "Ability \"" + abilityName + "\" is missing an AbilityPlacement.");
-        Assert.IsTrue(effects.Count == effectsTiming.Count, "Effects list for \"" + abilityName + "\" needs to match timing list length");
-        Assert.IsTrue(gameObjects.Count == gameObjectTiming.Count, "GameObjects list for \"" + abilityName + "\" needs to match timing list length");
-
-        // Run start on necessary parts
-        placement.Start();
-        placement.SetParent(this);
+        for(int i = 0; i < placements.Count; ++i){
+            placements[i] = UnityEngine.Object.Instantiate(placements[i]);
+            placements[i].Start();
+            placements[i].SetParent(this);
+        }
 
         // Setting up lists of effects and gameobjects
         effectsIndex = 0;
         gameObjectsIndex = 0;
 
-        // probably should at least assert to see if they're sorted...
-        // also assert if the last element in timing's is less than the cooldown... that would cause bugs
+        if(debug){
+            Assert.IsTrue(placements.Count > 0, "Ability \"" + abilityName + "\" is missing an AbilityPlacement.");
+            Assert.IsTrue(effects.Count == effectsTiming.Count, "Effects list for \"" + abilityName + "\" needs to match timing list length");
+            Assert.IsTrue(gameObjects.Count == gameObjectTiming.Count, "GameObjects list for \"" + abilityName + "\" needs to match timing list length");
+
+            // Check to see if the timings are sorted
+            for(int i = 1; i < effectsTiming.Count; ++i){
+                Assert(effectsTiming[i] >= effectsTiming[i-1], "Effects Timing is not sorted for ability " + abilityName);
+            }
+
+            for(int i = 1; i < gameObjectTiming.Count; ++i){
+                Assert(gameObjectTiming[i] >= gameObjectTiming[i-1], "Effects Timing is not sorted for ability " + abilityName);
+            }
+
+            // the timings must occur within the cooldown
+            Assert(effectsTiming[effectsTiming.Count -1] <= cooldown, "Timings must be less than or equal to the cooldown of the ability " + abilityName);
+            Assert(gameObjectTiming[gameObjectTiming.Count -1] <= cooldown, "Timings must be less than or equal to the cooldown of the ability " + abilityName);
+        }
     }
 
     public void Update(){
         currentCooldown += Time.deltaTime;
 
-        placement.Update();
+        foreach(AbilityPlacement placement in placements){
+            placement.Update();
+        }
 
         // If we're notified, wait for right or left click to either cast or cancel
         if(state == AbilityState.Notified){
-
             if(Input.GetMouseButtonDown(0) && currentCooldown > cooldown){
                 currentCooldown = 0.0f;
 
@@ -90,27 +107,30 @@ public class Ability : ScriptableObject {
                 effectsIndex = 0;
                 gameObjectsIndex = 0;
 
-                castPosition = placement.splat.transform.position;
+                // TODO just get mouse position into world...
+                castPosition = placements[0].splat.transform.position;
 
                 state = AbilityState.Casted;
             } else if(Input.GetMouseButtonDown(1)){
                 state = AbilityState.Idle;
             }
         }
-        // If we're casted, iterate up our effects/gameobjects list
+        // If we're casted, get all the targets from all placements and cast effects on said targets
         else if(state == AbilityState.Casted){
             // iterate through remaining effects and cast on targets at time specified
             for(int i = effectsIndex; i < effects.Count; ++i){
                 if(effectsTiming[i] < currentCooldown){
-                    Actor[] targets = placement.GetTargetsInCast(castPosition);
+                    foreach(AbilityPlacement placement in placements){
+                        Actor[] targets = placement.GetTargetsInCast(castPosition);
 
-                    effectsIndex++;
-                    foreach(Actor actor in targets){
-                        if(!actor || (!includeSelf && actor == selfActor)){
-                            continue;
+                        effectsIndex++;
+                        foreach(Actor actor in targets){
+                            if(!actor || (!includeSelf && actor == selfActor)){
+                                continue;
+                            }
+
+                            effects[i].Apply(actor);
                         }
-
-                        effects[i].Apply(actor);
                     }
                 }
             }
@@ -125,13 +145,15 @@ public class Ability : ScriptableObject {
     }
 
     public void Notify(){
-        if(placement.type > AbilityPlacement.PlacementType.ONHOTKEY &&
-           placement.type < AbilityPlacement.PlacementType.ONCLICK){
-            // If ability requires hotkey only
-            state = AbilityState.Casted;
-        } else if(placement.type > AbilityPlacement.PlacementType.ONCLICK){
-            // If ability requires a left click
-            state = AbilityState.Notified;
+        foreach(AbilityPlacement placement in placements){
+            if(placement.type > AbilityPlacement.PlacementType.ONHOTKEY &&
+               placement.type < AbilityPlacement.PlacementType.ONCLICK){
+                // If ability requires hotkey only
+                state = AbilityState.Casted;
+            } else if(placement.type > AbilityPlacement.PlacementType.ONCLICK){
+                // If ability requires a left click
+                state = AbilityState.Notified;
+            }
         }
     }
 
