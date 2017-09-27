eb93bd0485964f4b387f69bd1716c688ad0116af
Trevor Westphal <trevorawestphal@gmail.com>
2017-09-26 21:57:01 -0700

    Added dust particles that kick up when the character lands

---
+++ Assets/Scripts/Components/DustParticleComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (ParticleSystem))]
+public class DustParticleComponent : MonoBehaviour {
+
+    public GameObject character;
+
+    private bool wasGrounded = true;
+
+    private ParticleSystem particles;
+
+	void Start(){
+        particles = GetComponent<ParticleSystem>();
+	}
+
+	void Update(){
+        bool isGrounded = character.GetComponent<CharacterController>().isGrounded;
+
+        if(isGrounded && !wasGrounded){
+            particles.Play();
+        }
+
+        wasGrounded = isGrounded;
+	}
+}
