7911ad4aa60ecc3381c030ab987459d4dba6aef4
Trevor Westphal <trevorawestphal@gmail.com>
2017-09-28 22:05:46 -0700

    Cleanup. Also, commit 100

--- Assets/Scripts/Libs/Strings.cs
+++ Assets/Scripts/Libs/Strings.cs
@@ -34,10 +34,11 @@ public static class Strings {
     public enum LocalizedString {
         DefaultString,
         // Desolation
-        Des_HistoryOfCastle1,
-        Des_HistoryOfCastle2,
-        Des_HistoryOfCastle3,
-        Des_HistoryOfCastle4,
+        DesolationHistoryOfCastle1,
+        DesolationHistoryOfCastle2,
+        DesolationHistoryOfCastle3,
+        DesolationPatrolReport,
+        DesolationWandInformation,
     }
 
     static string[] stringDatabase = new string[]{
@@ -47,6 +48,7 @@ public static class Strings {
         "History of Fort Tanisan, season of the Owl\nThere have been reports of strange noises coming from the dungeon, of trinkets missing and food stolen. No soldiers have yet confessed, but I remain wary.",
         "History of Fort Tanisan, season of the Manticore\nAt last, rain! Praise be to Shano, Keeper of the Heavens. Thus ends a too long and thirsty year. I think it odd, last years supply should have lasted long into next season. Thirsty is the desert, indeed.",
         "Patrol Report for Fort Tanisan\nOur partrol found several giant craters in the sand. It may yet be related to the strange shifting of dunes we sometimes see in the distance. We should secure the hidden dungeons of Tanisan forthwith. Praise Shano, and the City She Lives In, The City Of Angels",
+        "...fortunately, among the treasures sent to the Fort to guard was the mighty wand FlameRuin. I have not witnessed it, but I have heard tell of it's power. MAGIC WAND ITEM DESCRIPTION HERE",
     };
 
     public static string GetString(LocalizedString stringEnum){
