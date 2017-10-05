fdc269ae47304b2a21085c4c43bf7f5e44aa683e
Trevor Westphal <trevorawestphal@gmail.com>
2017-10-05 08:12:31 -0700

    Covered desolation level in flat temp color, setup particle managing script

---
+++ Assets/Scripts/Components/ParticleVolumeComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (ParticleSystem))]
+[RequireComponent (typeof (BoxCollider))]
+public class ParticleVolumeComponent : MonoBehaviour {
+
+    private ParticleSystem particles;
+
+	void Start(){
+        particles = GetComponent<ParticleSystem>();
+        particles.Pause();
+	}
+
+	void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            particles.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.tag == "Player"){
+            particles.Pause();
+        }
+    }
+}
