422a381770ebff14c69073dad1b35c17a6f53622
Trevor Westphal <trevorawestphal@gmail.com>
2017-11-10 08:18:34 -0800

    Animation working for the most part! now for actual good animations

--- Assets/Scripts/Utilities/AnimationRecordUtility.cs
+++ Assets/Scripts/Utilities/AnimationRecordUtility.cs
@@ -3,80 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class AnimationRecordUtility : EditorWindow {
-    [MenuItem ("Window/My Window")]
+[ExecuteInEditMode]
+public class AnimationRecordUtility : MonoBehaviour {
+    public bool record;
+    public bool load;
+    public bool mirror;
 
-    public static void  ShowWindow () {
-        EditorWindow.GetWindow(typeof(AnimationRecordUtility));
+    public AnimationPose recordPose;
+
+    public GameObject[] bones;
+
+    public int[] mirrorIndices;
+
+    public void Start(){
+        record = false;
+        load = false;
+        mirror = false;
+
+        recordPose = null;
     }
 
-    void OnGUI() {
-        if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
-            Debug.Log("Clicked the button with text");
+    public void Update(){
+        if(record){
+            Debug.Log("SAVING POSE");
+            for(int i = 0; i < bones.Length; ++i){ // something's fucked up with this...
+                if(i == 0){
+                    Debug.Log(bones[i].transform.localRotation);
+                }
+                recordPose.joints[i] = bones[i].transform.localRotation;
+            }
+            
+            recordPose = null;
+            record = false;
+        }
+
+        if(load){
+            Debug.Log("LOADING POSE");
+
+            for(int i = 0; i < bones.Length; ++i){
+                bones[i].transform.localRotation = recordPose.joints[i];
+            }
+            load = false;
+        }
+
+        // if(mirror){
+        //     Debug.Log("MIRRORING POSE");
+        //     Quaternion[] newBonesrotation = new Quaternion[bones.Length];
+        //
+        //     for(int i = 0; i < bones.Length; ++i){
+        //         Quaternion oldrot = bones[mirrorIndices[i]].transform.rotation;
+        //
+        //         oldrot.w *= -1.0f;
+        //         oldrot.y *= -1.0f;
+        //
+        //         newBonesrotation[i] = oldrot;
+        //     }
+        //
+        //     for(int i = 0; i < bones.Length; ++i){
+        //         bones[i].transform.rotation = newBonesrotation[i];
+        //     }
+        //
+        //     mirror = false;
+        // }
     }
 }
-
-// public class AnimationRecordUtility : MonoBehaviour {
-//     public bool record;
-//     public bool load;
-//     public bool mirror;
-//
-//     public AnimationPose recordPose;
-//
-//     public GameObject[] bones;
-//
-//     public int[] mirrorIndices;
-//
-//     public void Start(){
-//         record = false;
-//         load = false;
-//         mirror = false;
-//
-//         recordPose = null;
-//     }
-//
-//     public void Update(){
-//         if(record){
-//             Debug.Log("SAVING POSE");
-//             for(int i = 0; i < bones.Length; ++i){ // something's fucked up with this...
-//                 recordPose.joints[i] = bones[i].transform.localRotation;
-//             }
-//
-//             SetDirty(recordPose);
-//             recordPose = null;
-//
-//             record = false;
-//         }
-//
-//         if(load){
-//             Debug.Log("LOADING POSE");
-//
-//             AnimationPose newPose = ScriptableObject.Instantiate(recordPose);
-//
-//             for(int i = 0; i < bones.Length; ++i){
-//                 bones[i].transform.localRotation = newPose.joints[i];
-//             }
-//             load = false;
-//         }
-//
-//         // if(mirror){
-//         //     Debug.Log("MIRRORING POSE");
-//         //     Quaternion[] newBonesrotation = new Quaternion[bones.Length];
-//         //
-//         //     for(int i = 0; i < bones.Length; ++i){
-//         //         Quaternion oldrot = bones[mirrorIndices[i]].transform.localRotation;
-//         //
-//         //         oldrot.w *= -1.0f;
-//         //         oldrot.y *= -1.0f;
-//         //
-//         //         newBonesrotation[i] = oldrot;
-//         //     }
-//         //
-//         //     for(int i = 0; i < bones.Length; ++i){
-//         //         bones[i].transform.localRotation = newBonesrotation[i];
-//         //     }
-//         //
-//         //     mirror = false;
-//         // }
-//     }
-// }
